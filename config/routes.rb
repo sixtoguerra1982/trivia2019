bee1066ac3617718286048ae12eab66bfffd2048
sixto.guerra1982@gmail.com <sixto.guerra1982@gmail.com>
2019-07-11 10:04:16 -0400

    Devise Model User

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+     sessions: 'users/sessions'
+   }
   get 'pages/index'
-  root 'pages#index'
+  root to: 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
