700d50f2c29704b58791bfe16c1cfeb16b310c6f
sixto.guerra1982@gmail.com <sixto.guerra1982@gmail.com>
2019-07-11 11:42:39 -0400

    navbar resource question

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_11_142215) do
+ActiveRecord::Schema.define(version: 2019_07_11_150251) do
+
+  create_table "questions", force: :cascade do |t|
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
