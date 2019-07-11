526f4e384da92ede18ed052b8cb06dd78e380bed
sixto.guerra1982@gmail.com <sixto.guerra1982@gmail.com>
2019-07-11 10:47:10 -0400

    add name to user and open strongs params devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-     sessions: 'users/sessions'
-   }
+   devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   get 'pages/index'
   root to: 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
