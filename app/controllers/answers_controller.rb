340f6b6cdd2fabe2654d3b6587c9dab2bce40e82
sixto.guerra1982@gmail.com <sixto.guerra1982@gmail.com>
2019-07-11 16:10:53 -0400

    limit of answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -8,13 +8,19 @@ class AnswersController < ApplicationController
     @answer = Answer.new
   end
   def create
+    @limit_answers = 5
     @answer = Answer.new(answer_params)
     respond_to do |format|
-      if @answer.save
-        format.html { redirect_to answers_path , notice: 'Answer was successfully created.' }
+      @answers = Answer.where(question_id: @answer.question_id)
+      if @answers.count < @limit_answers
+        if @answer.save
+          format.html { redirect_to answers_path , notice: 'Answer was successfully created.' }
+        else
+          format.html { render :new }
+          format.json { render json: @answer.errors, status: :unprocessable_entity }
+        end
       else
-        format.html { render :new }
-        format.json { render json: @answer.errors, status: :unprocessable_entity }
+        format.html { redirect_to answers_path , alert: 'The maximum number of responses has been exceeded' }
       end
     end
   end
