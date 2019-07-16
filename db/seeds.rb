8116480dda3c994138f3a95a6aac4df457bd7a42
sixto.guerra1982@gmail.com <sixto.guerra1982@gmail.com>
2019-07-16 01:57:58 -0400

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(email: "admin@trivia.cl", name: "Administrador", password: "admin123", rol: 0)
+User.create!(email: "sixto@gmail.com", name: "Sixto Guerra", password: "123123")
+10.times do |i|
+  Question.create!(description: "#{i+1} + #{i+1}")
+  Answer.create!(answer: (i + 1) + (i + 1) , question_id: i+1 , tipo: true)
+  4.times do |x|
+    Answer.create!(answer: (x+1 * i+1) * 3 , question_id: i+1 , tipo: false)
+  end
+end
