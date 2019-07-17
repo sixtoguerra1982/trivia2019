5ea5c1e39abd9ab78892f252c8ec877d6d1f505f
sixto.guerra1982@gmail.com <sixto.guerra1982@gmail.com>
2019-07-16 20:19:17 -0400

    fin

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,26 @@
 class PagesController < ApplicationController
   def index
-    @questions = Question.all
-    @answerscant = @questions.count
 
-    sql= 'SELECT questions.id AS Q_ID, questions.description, questions.created_at, questions.updated_at, answers.id AS A_ID, answers.answer, answers.tipo, answers.created_at, answers.updated_at, answersusers.id AS AU_ID, answersusers.value, answersusers.created_at, answersusers.updated_at, users.email, users.encrypted_password, users.reset_password_token, users.reset_password_sent_at, users.remember_created_at, users.created_at, users.updated_at, users.name, users.role, users.id AS U_ID FROM questions LEFT JOIN answers ON answers.question_id = questions.id LEFT JOIN answersusers ON answersusers.answer_id = answers.id LEFT JOIN users ON answersusers.user_id = users.id where au_id is NULL'
-    @ranking = ActiveRecord::Base.connection.execute(sql)
+     filtro={}
+     if user_signed_in?
+       sql = 'SELECT u.id AS UDI, u.name, q.id AS QID, q.description, (SELECT x.id FROM answers x INNER JOIN answersusers as au ON au.answer_id = x.id where au.user_id=u.id and x.question_id=q.id ) as RESPUESTA FROM users AS u , questions AS q where UDI=' + current_user.id.to_s + " limit 5"
+       filtro = ActiveRecord::Base.connection.execute(sql)
+     end
+
+     @filtrados = []
+     filtro.each do |f|
+       if f['RESPUESTA'].nil?
+         @filtrados.push f['QID']
+       end
+     end
+
+     @answerscant = @filtrados.size
+
+
   end
 
   def ranking
     sql = "Select user_id, count(value) as cantidad from Answersusers where answersusers.value = '1' group by user_id order by cantidad desc limit 3"
     @ranking = ActiveRecord::Base.connection.execute(sql)
-    byebug
   end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,4 +2,7 @@ module QuestionsHelper
   def answersOfQuestion(question)
     Answer.where(question_id: question)
   end
+  def questionsearch(cuestion_id)
+    Question.find(cuestion_id)
+  end
 end
